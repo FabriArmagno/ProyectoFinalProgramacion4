b52d532ad5230b437df2e1562d5bc03a0320a488
Fabri <milrayas7@gmail.com>
2025-10-17 12:07:24 -0300

    first commit

---
+++ src/main/java/com/Proyecto/Final/Programacion4/FabricioArmagno/FabricioArmagnoApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto.Final.Programacion4.FabricioArmagno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricioArmagnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricioArmagnoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Proyecto/Final/Programacion4/FabricioArmagno/FabricioArmagnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto.Final.Programacion4.FabricioArmagno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricioArmagnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
